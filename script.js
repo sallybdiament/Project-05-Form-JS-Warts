b875cc4670a48edbdd3be887b82f1ac00e680acb
thais <thaisviana081@outlook.com>
2022-05-06 09:15:21 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const botaoLogin = document.getElementById('botao');
 const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('senha');
+const checkBox = document.getElementById('agreement');
+const btnEnviar = document.getElementById('submit-btn');
 
 botaoLogin.addEventListener('click', () => {
   if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
     alert('Olá, Tryber!');
   }
   alert('Email ou senha inválidos.');
+});
+
+btnEnviar.disabled = true;
+checkBox.addEventListener('click', () => {
+  if (checkBox.checked) {
+    btnEnviar.disabled = false;
+  }
 });
