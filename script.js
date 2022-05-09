5352174da7b1a69d9d2c3b35b2a208a3f9157ea8
Sally Diament <sally.barzilai@gmail.com>
2022-05-09 13:53:21 -0300

    incluindo requisito 20 bônus e mexendo um pouco nas margens

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('senha');
 const checkBox = document.getElementById('agreement');
 const btnEnviar = document.getElementById('submit-btn');
+const contador = document.getElementById('counter');
+const textArea = document.getElementById('textarea')
 
 botaoLogin.addEventListener('click', () => {
   if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
@@ -16,4 +18,15 @@ checkBox.addEventListener('click', () => {
   if (checkBox.checked) {
     btnEnviar.disabled = false;
   }
+});
+
+// Fonte para fazer o requisito 20 do bônus: 
+// 1) https://www.youtube.com/watch?v=sRlgAjlwz0o&ab_channel=MatheusBattisti-HoradeCodar 
+// 2) https://pt.stackoverflow.com/questions/25753/como-fazer-um-contador-de-caracteres-de-uma-textarea
+
+contador.innerHTML = 500;
+textArea.addEventListener('input', () => {
+  const tamanhoContador = textArea.value.length;
+  contador.innerHTML = 500 - tamanhoContador;
+  // contador.innerHTML -= 1;
 });
